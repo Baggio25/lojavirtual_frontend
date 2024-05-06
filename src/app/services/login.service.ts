7564d766f6999fbcda4f1ce26feab7d6a2dfe85e
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2024-05-05 22:59:30 -0300

    Criado login.service, ajustado url da api

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environent';
+import { Usuario } from '../model/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private ulrApi = environment.urlApi + '/login';
+
+  constructor(private http: HttpClient) { 
+  }
+
+  logar( usuario: Usuario) {
+    return this.http.post<string>(this.ulrApi, usuario);
+  }
+
 }
